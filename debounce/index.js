28a598e4a680ab3ca8d8f0b8e0df5dcbd0bfe8c9
Sergey Shevtsov <sergioshev@gmail.com>
2018-06-05 12:44:30 +0400

    Added promised version

--- debounce/index.js
+++ debounce/index.js
@@ -33,10 +33,69 @@ function debounce(callback, timeOut, inmediate = false) {
   };
 }
 
-const dconsole = debounce(console.log, 50);
+function debouncePromised(callback, timeOut, inmediate = false) {
+  let timerId ;
+  let lastArgs;
+  let deferedPromise;
+
+  const resolveDelayedPromise = function() {
+    Promise.resolve(callback.apply(this, lastArgs))
+      .then(deferedPromise.res, deferedPromise.rej);
+    deferedPromise = null;
+    lastArgs = null;
+    timerId = null;
+  };
+
+  return function() {
+    let context = this, args = arguments;
+    let shouldCall = !timerId && !lastArgs && inmediate;
+    lastArgs = args;
+    if (shouldCall) return Promise.resolve(callback.apply(this, args));
+
+    clearTimeout(timerId);
+    timerId = setTimeout(resolveDelayedPromise.bind(this), timeOut);
+    
+    if (!deferedPromise) {
+      deferedPromise = {};
+      deferedPromise.promise = new Promise((res, rej) => {
+        deferedPromise.res = res;
+        deferedPromise.rej = rej;
+      });
+    }
+    return deferedPromise.promise;
+  };
+}
+
+
+/*
+const dconsole = debounce((r) => r, 50);
 
 for (let j=0; j<100 ; j++){
  // setTimeout(() => { dconsole(j, j+1) }, 50*j) ;
-  setTimeout(() => { dconsole(j, j+1) }, 50*j) ;
+  setTimeout(() => { v = dconsole(j) ; console.log(v) ; }, 50*j) ;
 }
+*/
+
+const someFunction = (v) => {
+  return new Promise((res, rej) => {
+    const delay = Math.floor(Math.random()*19+1);
+    console.log(`Doing job for ${delay} ms`);
+    setTimeout(() => res([v, delay]), delay);
+  });
+}
+
+const debouncedFunction = debouncePromised(someFunction, 40, true);
+
+let array = [];
+for (let j=1; j<10 ; j++) {
+  array.push(j);
+}
+
+array.forEach(e => {
+  return debouncedFunction(e).then(v => console.log(`Resolved with ${v}`));
+});
+
+
+
+
 
